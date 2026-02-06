f1967b73525f4889496ea588e831a0861baf5889
GraphZLL <graphzou@gmail.com>
2026-02-06 23:52:27 +0800

    Update test_half.cpp

--- src/tests/test_half.cpp
+++ src/tests/test_half.cpp
@@ -61,26 +61,32 @@ int main() {
     Buffer<float3> buffer = device.create_buffer<float3>(1);
 
 
+
+
     Kernel kernel = [&](Float f) {
 //        $info("{} ", f);
-//        sqrt(Half{});
+        sqrt(Half{});
 //        Uint3 a = make_uint3(3);
 //        Int3 b ;
 //        b = a;
 
-        float3 f3 = make_float3(0.8f,0.5f, 1.f);
-        half3 h3 = make_float3(1.f, 2.f, 3.f);
-        buffer.write(0, f3);
-        float4 f4 = make_float4(h3,half( 1.f));
-        h3 = f3;
+        Float3 f3 = make_float3(1.f,5.f, 9.f);
+        Half3 h3 = make_float3(1.f, 2.f, 3.f);
+//        buffer.write(0, h3);
+//        Float4 f4 = make_float4(h3,half( 1.f));
+//        h3 = f3;
 
-        auto a3 = ocarina::select(make_bool3(1,0,1).xyz(), uint3{5,6,7}, float3{1,2,3}.xyz());
+        h3 = f3.xyz();
 
-        auto re = f3.x * h3.xyz() ;
+//        auto a3 = ocarina::select(make_bool3(1,0,1).xyz(), uint3{5,6,7}, float3{1,2,3}.xyz());
+
+//        auto re = f3.x * h3.xyz() ;
 
 //        re = h3.xxx();
 
-        $info("---  {} {} {} {}", f4);
+//      $info("------  {} {} {} ", Uint3{});
+      $info("---  {} {} {} ", sqrt(h3) );
+      $info("------==  {} {} {} ", Uint3{});
     };
     auto shader = device.compile(kernel);
     stream << shader(6.f).dispatch(1) << Env::printer().retrieve();
