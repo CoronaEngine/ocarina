27c0e0ec2bd371637324932e59d755f11a296328
GraphZLL <graphzou@gmail.com>
2026-02-07 15:19:53 +0800

    Update cuda_device_vector.h

--- src/backends/cuda/builtin/cuda_device_vector.h
+++ src/backends/cuda/builtin/cuda_device_vector.h
@@ -359,43 +359,27 @@ namespace ocarina {
 
 namespace detail {
 
-template<size_t N, typename P, typename T, typename F, size_t... i>
-[[nodiscard]] constexpr auto select_helper(Vector<P, N> pred, Vector<T, N> t, Vector<F, N> f, ocarina::index_sequence<i...>) {
+template<size_t N, template<typename, size_t> typename Container, typename P, typename T, typename F, size_t... i>
+[[nodiscard]] constexpr auto select_helper(Container<P, N> pred, Container<T, N> t, Container<F, N> f, ocarina::index_sequence<i...>) {
     using scalar_type = decltype(ocarina::select(bool{}, T{}, F{}));
-    return Vector<scalar_type, N>{ocarina::select(pred[i], t[i], f[i])...};
-}
-
-template<size_t N, typename P, typename T, typename F, size_t... i>
-[[nodiscard]] constexpr auto select_helper(array<P, N> pred, array<T, N> t, array<F, N> f, ocarina::index_sequence<i...>) {
-    using scalar_type = decltype(ocarina::select(bool{}, T{}, F{}));
-    return array<scalar_type, N>{ocarina::select(pred[i], t[i], f[i])...};
+    return Container<scalar_type, N>{ocarina::select(pred[i], t[i], f[i])...};
 }
 
 }// namespace detail
 
-template<size_t N, typename P, typename T, typename F>
-[[nodiscard]] constexpr auto select(Vector<P, N> pred, Vector<T, N> t, Vector<F, N> f) {
+template<size_t N, template<typename, size_t> typename Container, typename P, typename T, typename F>
+[[nodiscard]] constexpr auto select(Container<P, N> pred, Container<T, N> t, Container<F, N> f) {
     return detail::select_helper(pred, t, f, ocarina::make_index_sequence<N>());
 }
 
-template<size_t N, typename P, typename T, typename F>
-[[nodiscard]] constexpr auto select(P pred, Vector<T, N> t, Vector<F, N> f) {
-    return select(Vector<P, N>(pred), t, f);
-}
-
-template<size_t N, typename P, typename T, typename F>
-[[nodiscard]] constexpr auto select(array<P, N> pred, array<T, N> t, array<F, N> f) {
-    return detail::select_helper(pred, t, f, ocarina::make_index_sequence<N>());
-}
-
-template<size_t N, typename P, typename T, typename F>
-[[nodiscard]] constexpr auto select(P pred, array<T, N> t, array<F, N> f) {
-    return select(array<P, N>(pred), t, f);
+template<size_t N, template<typename, size_t> typename Container, typename P, typename T, typename F>
+[[nodiscard]] constexpr auto select(P pred, Container<T, N> t, Container<F, N> f) {
+    return select(Container<P, N>(pred), t, f);
 }
 
 }// namespace ocarina
 
-template<typename ...Args>
+template<typename... Args>
 [[nodiscard]] constexpr auto oc_select(Args... args) noexcept { return ocarina::select(args...); }
 
 //#undef OC_MAKE_TYPE_N
