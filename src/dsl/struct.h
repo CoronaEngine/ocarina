0e697e002b37dfc5e35cc0b418d17293e0238dcc
GraphZLL <graphzou@gmail.com>
2026-04-13 16:06:33 +0800

    Update struct.h

--- src/dsl/struct.h
+++ src/dsl/struct.h
@@ -15,6 +15,7 @@
 
 #define OC_STRUCT_IMPL(NS, S, ...)                                            \
     OC_MAKE_STRUCT_REFLECTION(NS::S, ##__VA_ARGS__)                           \
+    OC_MAKE_STORAGE_TYPE(NS::S, ##__VA_ARGS__)                                \
     OC_MAKE_STRUCT_DESC(NS::S, ##__VA_ARGS__)                                 \
     OC_MAKE_STRUCT_IS_DYNAMIC(NS::S, ##__VA_ARGS__)                           \
     OC_MAKE_COMPUTABLE_BODY(NS::S, ##__VA_ARGS__)                             \
